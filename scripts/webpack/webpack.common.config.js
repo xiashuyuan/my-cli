a55c2e5c71a606785e7b2f37cc6a7e16306e3251
xiashuyuan <xsy1213soss@163.com>
2021-08-30 14:42:10 +0800

    安装 eslint 相关依赖，配置ESLintPlugin 集成 react-app、prettier 风格

--- scripts/webpack/webpack.common.config.js
+++ scripts/webpack/webpack.common.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 
 const CleanTerminalPlugin = require('clean-terminal-webpack-plugin')
 const ProgressBarPlugin = require('webpackbar')
+const ESLintPlugin = require('eslint-webpack-plugin')
 
 module.exports = {
   entry: { index: path.resolve(__dirname, '../../src/index.tsx') },
@@ -32,7 +33,13 @@ module.exports = {
       },
     ]
   },
-  plugins: [new CleanTerminalPlugin(), new ProgressBarPlugin()],
+  plugins: [
+    new CleanTerminalPlugin(),
+    new ProgressBarPlugin(),
+    new ESLintPlugin({
+      extensions: ['js', 'jsx', 'ts', 'tsx']
+    })
+  ],
   optimization: {
     usedExports: true
   }
